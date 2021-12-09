ff67e26d3bc22d63aee8dce4cb3078b5988e7bf6
shubhamdas123 <87475413+shubhamdas123@users.noreply.github.com>
2021-12-09 17:15:33 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,59 @@
+function glow1() {
+    document.getElementById("star_0").src = "star_on.png";
+}
+
+function glow2() {
+    document.getElementById("star_0").src = "star_on.png";
+    document.getElementById("star_1").src = "star_on.png";
+}
+
+function glow3() {
+    document.getElementById("star_0").src = "star_on.png";
+    document.getElementById("star_1").src = "star_on.png";
+    document.getElementById("star_2").src = "star_on.png";
+}
+
+function glow4() {
+    document.getElementById("star_0").src = "star_on.png";
+    document.getElementById("star_1").src = "star_on.png";
+    document.getElementById("star_2").src = "star_on.png";
+    document.getElementById("star_3").src = "star_on.png";
+}
+
+function glow5() {
+    document.getElementById("star_0").src = "star_on.png";
+    document.getElementById("star_1").src = "star_on.png";
+    document.getElementById("star_2").src = "star_on.png";
+    document.getElementById("star_3").src = "star_on.png";
+    document.getElementById("star_4").src = "star_on.png";
+}
+
+function off1() {
+    document.getElementById("star_0").src = "star_off.png";
+}
+
+function off2() {
+    document.getElementById("star_0").src = "star_off.png";
+    document.getElementById("star_1").src = "star_off.png";
+}
+
+function off3() {
+    document.getElementById("star_0").src = "star_off.png";
+    document.getElementById("star_1").src = "star_off.png";
+    document.getElementById("star_2").src = "star_off.png";
+}
+
+function off4() {
+    document.getElementById("star_0").src = "star_off.png";
+    document.getElementById("star_1").src = "star_off.png";
+    document.getElementById("star_2").src = "star_off.png";
+    document.getElementById("star_3").src = "star_off.png";
+}
+
+function off5() {
+    document.getElementById("star_0").src = "star_off.png";
+    document.getElementById("star_1").src = "star_off.png";
+    document.getElementById("star_2").src = "star_off.png";
+    document.getElementById("star_3").src = "star_off.png";
+    document.getElementById("star_4").src = "star_off.png";
+}
